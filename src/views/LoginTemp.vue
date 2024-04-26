04b0d3bea0f26a56ab755a5eaf6c547de7bbc517
MrKeeplearning <fossilhunter@hanmail.net>
2024-04-26 18:48:52 +0900

    [Refactor] 로그인 화면 디자인 1차 수정

--- src/views/LoginTemp.vue
+++ src/views/LoginTemp.vue
@@ -6,28 +6,26 @@
       <v-col cols="12" sm="8" md="4">
         <v-card class="elevation-12">
           <v-toolbar class="custom-toolbar">
-            <v-toolbar-title class="title-padding">로그인</v-toolbar-title>
+            <v-toolbar-title>로그인</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
-            <v-form v-model="form" class="padded-form" @submit.prevent="onSubmit">
-              <v-card-title class="title-padding">이메일</v-card-title>
+            <v-form v-model="form" @submit.prevent="onSubmit">
+              <v-card-title>이메일</v-card-title>
               <v-text-field
                   name="email"
                   placeholder="이메일을 입력하세요."
                   type="email"
-                  color="#35964a"
                   clearable
                   v-model="email"
                   :readonly="loading"
                   :rules="[rules.required, rules.email]"
                   variant="outlined"
-                  density="comfortable"
+                  class="padded-form"
               />
-              <v-card-title class="title-padding">비밀번호</v-card-title>
+              <v-card-title>비밀번호</v-card-title>
               <v-text-field
                   name="password"
                   placeholder="비밀번호를 입력하세요."
-                  color="#35964a"
                   clearable
                   v-model="password"
                   :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
@@ -36,44 +34,49 @@
                   :rules="[rules.required]"
                   @click:append-inner="visible = !visible"
                   variant="outlined"
-                  density="comfortable"
+                  class="padded-form"
               />
               <v-btn
                   :disabled="!form"
                   :loading="loading"
-                  color="#0f2a14"
+                  color="#000000"
                   size="large"
                   type="submit"
                   variant="elevated"
                   block
-                  class="my-6"
               >
                 로그인
               </v-btn>
-              <v-divider></v-divider>
-              <v-btn
-                  class="register-button my-6"
-                  block
-                  variant="outlined"
-                  size="large"
-                  color="#0f2a14"
-                  @click="$router.push({name:'register'})"
-              >
-                회원가입
-              </v-btn>
-              <v-btn
-                  class="register-button mt-2"
-                  block
-                  variant="outlined"
-                  color="#0f2a14"
-                  size="x-large"
-                  prepend-icon="mdi-google"
-                  @click="googleLogin"
-              >
-                구글 계정으로 계속하기
-              </v-btn>
             </v-form>
           </v-card-text>
+          <v-form v-model="form" @submit.prevent="onSubmit">
+
+            <v-btn
+                :disabled="!form"
+                :loading="loading"
+                color="#000000"
+                size="large"
+                type="submit"
+                variant="elevated"
+                block
+            >
+              로그인
+            </v-btn>
+            <v-btn color="blue" class="mt-3">
+              <router-link to="register">회원가입</router-link>
+            </v-btn>
+            <v-btn color="red" class="mt-2" block @click="googleLogin">
+              구글 로그인
+            </v-btn>
+            <v-btn
+                color="yellow darken-1"
+                class="mt-2"
+                block
+                @click="kakaoLogin"
+            >
+              카카오 로그인
+            </v-btn>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
@@ -127,7 +130,7 @@ export default {
         if (accessToken && refreshToken) {
           localStorage.setItem("accessToken", accessToken);
           localStorage.setItem("refreshToken", refreshToken);
-          this.$router.push({name: "MainPage"});
+          this.$router.push({ name: "MainPage" });
         } else {
           console.log("200 OK But Not Token");
           alert("로그인 실패");
@@ -152,15 +155,6 @@ export default {
 .padded-form {
   padding-left: 16px;
   padding-right: 16px;
-}
-
-.title-padding {
-  padding-left: 0;
-}
-
-.register-button {
-  color: #2b783b !important; /* 내부 텍스트 색상 */
-  border-color: #e0e0e0 !important; /* 외곽선 색상 */
 }
 
 .login-form {

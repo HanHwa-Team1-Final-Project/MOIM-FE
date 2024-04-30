8af409d9762ab423a22f0d5807468ce6da534ee2
MrKeeplearning <fossilhunter@hanmail.net>
2024-04-30 18:04:30 +0900

    [Feat] 채팅 화면 수정 중

--- src/views/ChatTest.vue
+++ src/views/ChatTest.vue
@@ -2,7 +2,7 @@
   <div class="d-flex flex-column">
     <div v-for="(msg, id) in messages" :key="id" class="mb-3 mx-auto text-start">
       <v-card color="blue lighten-5" class="py-2 px-3" style="width: fit-content; max-width: 400px" outlined>
-        {{ msg.message }}
+        {{ msg.content }} <!-- msg.message에서 msg.content로 변경 -->
       </v-card>
     </div>
   </div>
@@ -19,29 +19,42 @@ import { Stomp } from '@stomp/stompjs'
 
 export default {
   setup() {
-    // Data
     const messages = ref([])
     const stompClient = ref(null)
     const message = ref('')
 
-    // Methods
     const initializeWebSocket = () => {
-      const socket = new SockJS('http://localhost:8080/ws-endpoint')
+      const authToken = localStorage.getItem("accessToken");
+      console.log("start")
+      console.log("토큰 정보", authToken);
+
+      const socket = new SockJS(`http://localhost:8080/ws-endpoint`)
       stompClient.value = Stomp.over(socket)
 
-      stompClient.value.connect({}, () => {
-        stompClient.value.subscribe('/sub/chat', (response) => {
-          const msg = JSON.parse(response.body)
-          messages.value.push(msg)
-        })
+      stompClient.value.connect({
+        'Authorization': `Bearer ${authToken}`
+      }, () => {
+        console.log('Connected to WebSocket');
+        stompClient.value.subscribe('/sub/chat', (response) => { // 경로 수정
+          const msg = JSON.parse(response.body);
+          console.log("msg 어떻게 생김?", msg);
+          messages.value.push(msg);
+        });
       }, (error) => {
-        console.error('WebSocket connection error:', error)
-      })
+        console.error('WebSocket connection error:', error);
+      });
     }
 
     const sendMessage = () => {
-      if(stompClient.value && stompClient.value.connected) {
-        const messageData = { message: message.value }
+      if (stompClient.value && stompClient.value.connected) {
+        // messageData 객체의 구조를 백엔드와 일치시키기
+        const messageData = {
+          type: "CHAT", // 메시지 타입 지정
+          content: message.value, // content 필드 사용
+          sender: "User" // 임시로 사용자 이름 하드코딩
+        }
+        
+        console.log("메시지 형식!!!", messageData);
 
         stompClient.value.send('/pub/chat', {}, JSON.stringify(messageData))
         message.value = ''
@@ -50,16 +63,14 @@ export default {
       }
     }
 
-    // Lifecycle hooks
     onMounted(initializeWebSocket)
-
     onUnmounted(() => {
-      if(stompClient.value && stompClient.value.connected) {
+      if (stompClient.value && stompClient.value.connected) {
         stompClient.value.disconnect()
       }
     })
 
-    return { messages, message, sendMessage }
+    return {messages, message, sendMessage}
   }
 }
 </script>

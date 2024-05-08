bd8774fdb9c813b431c43b7689de026e638a17d6
qoth-0 <bny1324@naver.com>
2024-05-08 19:34:39 +0900

    오늘의일정 체크리스트

--- src/components/layout/AppSideBar.vue
+++ src/components/layout/AppSideBar.vue
@@ -69,42 +69,55 @@
           </v-list-item-content>
         </v-list-item> -->
 
-          <!-- 이벤트 및 투두 리스트 -->
-          <v-list-item class="schedule-item" v-if="!events.length == 0">
-            <v-list-item-content v-for="event in events" :key="event.id">
-              <v-list-item-title style="margin-top: 8px;"> {{ event.title }}</v-list-item-title>
-              <v-list-item-subtitle style="margin-bottom: -5px;"
-              >{{ formatTime(event.startDate) }} ~ {{ formatTime(event.endDate) }}
-              </v-list-item-subtitle>
-
-            <v-col v-if="event.todoLists.length">
-              <v-list-item-content v-for="todo in event.todoLists" :key="todo[0]">
-                <v-row style="height: 50px; margin-top:-15%">
-                  <v-col cols="12" md="2" class="mt-2 pa-0 d-flex align-center">
-                    <v-checkbox
-                      style="font-size: 12px;"
-                      v-model="todo[2]"
-                      :true-value="'Y'"
-                      :false-value="'N'"
-                      @change="updateIsChecked(todo[0], todo[2])"
-                    ></v-checkbox>
-                  </v-col>
-                  <v-col cols="12" md="10" class="mt-6 pa-0 pl-1" style="height: 10px;">
-                    <div :style="todo[2] === 'Y' ? 'text-decoration: line-through; text-decoration-color: #6d6d6d;' : ''">
-                      {{ todo[1] }}
-                    </div>
-                    <!-- <input type="text" readonly>{{ todo[1] }} -->
-                    <!-- <v-text-field variant="underlined" readonly>{{ todo[1] }}</v-text-field> -->
-                  </v-col>
-                </v-row>
-              </v-list-item-content>
-            </v-col>
-            <v-col  v-else></v-col>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item v-if="events.length == 0" style="text-align: center;">
+        <!-- 이벤트 및 투두 리스트 -->
+        <!-- 일정이 없을 경우 -->
+        <v-list-item v-if="events.length === 0" style="text-align: center;">
           일정이 없습니다.
         </v-list-item>
+
+        <!-- 일정이 있을 경우 -->
+        <template v-else>
+          <v-list-item class="schedule-item" v-for="(event, index) in events" :key="event.id">
+            <v-list-item-content>
+              <v-row>
+                <v-col cols="12" md="2" class="mt-2">
+                  <v-icon class="mr-2">mdi-chat-plus-outline</v-icon>
+                </v-col>
+                <v-col cols="12" md="10">
+                  <v-list-item-title >{{ event.title }}</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{ matrixToDescription(event.matrix) }}
+                  </v-list-item-subtitle>
+                </v-col>
+              </v-row>
+              <!-- 투두리스트 -->
+              <v-row v-if="event.todoLists.length > 0" style="margin-top: -10%; margin-left: 10%;">
+                <v-col cols="12">
+                  <v-list-item-content v-for="todo in event.todoLists" :key="todo[0]">
+                    <v-row style="height: 50px; margin-top:-15%">
+                      <v-col cols="12" md="2" class="mt-2 pa-0 d-flex align-center">
+                        <v-checkbox
+                          style="font-size: 12px;"
+                          v-model="todo[2]"
+                          :true-value="'Y'"
+                          :false-value="'N'"
+                          @change="updateIsChecked(todo[0], todo[2])"
+                        ></v-checkbox>
+                      </v-col>
+                      <v-col cols="12" md="10" class="mt-6 pa-0 pl-1" style="height: 10px;">
+                        <div :style="todo[2] === 'Y' ? 'text-decoration: line-through; text-decoration-color: #6d6d6d;' : ''">
+                          {{ todo[1] }}
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-list-item-content>
+                </v-col>
+              </v-row>
+            </v-list-item-content>
+            <!-- 이벤트 사이의 구분선 -->
+            <v-divider v-if="index < events.length - 1" class="mt-2"></v-divider>
+          </v-list-item>
+        </template>
       </v-list-item-group>
     </v-list>
     </v-col>
@@ -153,6 +166,20 @@ export default {
         return false;
       }
       return todo[2] === "Y";
+    },
+    matrixToDescription(matrix) {
+      switch (matrix) {
+          case 'Q1':
+              return '중요 & 긴급하지 않음';
+          case 'Q2':
+              return '중요 & 긴급';
+          case 'Q3':
+              return '긴급 & 중요하지 않음';
+          case 'Q4':
+              return '긴급하지 않음 & 중요하지 않음';
+          default:
+              return '';
+      }
     },
 
     async fetchUserData() {
